4c983531f19aa858d52dfd7eaab266412050cebd
Corey Ehmke <corey@idolhands.com>
2012-09-11 22:09:13 -0500

    Fix scoping

--- lib/faceted/presenter.rb
+++ lib/faceted/presenter.rb
@@ -18,7 +18,7 @@ module Faceted
 
       def build_association_from(field)
         bare_name = field.gsub("_id", "")
-        klass = eval "#{scope}::#{bare_name.classify}"
+        klass = eval "#{scope}#{bare_name.classify}"
         define_method :"#{bare_name}" do
           klass.new(:id => self.send(field))
         end
